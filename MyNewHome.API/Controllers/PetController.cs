494359cd4be8dc4070f990aa0a10573dc5eed542
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-10-31 13:33:57 +0100

    Storage használata a PetControllerben

--- MyNewHome.API/Controllers/PetController.cs
+++ MyNewHome.API/Controllers/PetController.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using MyNewHome.Bll;
 
 namespace MyNewHome.Controllers
@@ -15,10 +18,12 @@ namespace MyNewHome.Controllers
     public class PetController : ControllerBase
     {
         private readonly PetService _petService;
+        private readonly CloudStorageAccount _storage;
 
-        public PetController(PetService petService)
+        public PetController(PetService petService, IConfiguration configuration)
         {
             _petService = petService;
+            _storage = CloudStorageAccount.Parse(configuration["StorageConnectionString"]);
         }
 
         [HttpGet]
@@ -38,10 +43,46 @@ namespace MyNewHome.Controllers
         [HttpPost("upload")]
         public async Task<ActionResult> UploadAndRecognizeImage()
         {
-            // TODO save to blob
+            var image = Request?.Form?.Files?[0];
+            if (image == null) return BadRequest();
+
+            // Retrieve a reference to a container
+            var container = _storage.CreateCloudBlobClient().GetContainerReference("pets");
+
+            // Create the container if it doesn't already exist
+            await container.CreateIfNotExistsAsync();
+
+            // Set container access level
+            await container.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Container });
+
+            string ext = GetImageExtension(image.ContentType);
+            if (ext == null) return BadRequest();
+
+            // Upload image from stream with a generated filename
+            var blob = container.GetBlockBlobReference(Guid.NewGuid().ToString() + "." + ext);
+            await blob.UploadFromStreamAsync(image.OpenReadStream());
+
+            var url = blob.Uri.AbsoluteUri;
+
             // TODO recognize pet type
 
-            throw new NotImplementedException();
+            return Ok(new { url, type = "", probability = 0 });
+        }
+
+        private string GetImageExtension(string contentType)
+        {
+            switch (contentType)
+            {
+                case "image/png": return "png";
+                case "image/jpeg": return "jpeg";
+                case "image/jpg": return "jpg";
+                case "image/gif": return "gif";
+                case "image/bmp": return "bmp";
+                case "image/ief": return "ief";
+                case "image/svg+xml": return "svg+xml";
+                case "image/raw": return "raw";
+                default: return null;
+            }
         }
     }
 }
