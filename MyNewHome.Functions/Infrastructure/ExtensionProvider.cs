cc63d4097fd2dedcbdfa2fc3764fd1b190344da4
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-10-31 16:51:09 +0100

    function DI fix

---
+++ MyNewHome.Functions/Infrastructure/ExtensionProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Azure.WebJobs.Host.Config;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyNewHome.Functions
+{
+    internal class ExtensionProvider<TDependency, TAttribute> : IExtensionConfigProvider
+        where TAttribute : Attribute
+    {
+        private readonly TDependency _dependency;
+
+        public ExtensionProvider(TDependency dependency)
+        {
+            _dependency = dependency;
+        }
+
+        public void Initialize(ExtensionConfigContext context) => context.AddBindingRule<TAttribute>().BindToInput(_ => _dependency);
+    }
+}
