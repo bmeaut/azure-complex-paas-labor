7c943ea0b7075bf81a8b9c98f1c8fedfd12008b7
Tibor Tóth <toth.tibor@aut.bme.hu>
2019-10-31 11:21:22 +0100

    underscore

--- MyNewHome.Functions/NewPetFunction.cs
+++ MyNewHome.Functions/NewPetFunction.cs
@@ -14,13 +14,13 @@ namespace MyNewHome.Functions
 {
     public static class NewPetFunction
     {
-        private static readonly string cosmosConnectionString = Environment.GetEnvironmentVariable("CosmosConnectionString");
-        private static readonly string computerVisionApiKey = Environment.GetEnvironmentVariable("ComputerVision");
-        private static readonly string storageConnectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
+        private static readonly string _cosmosConnectionString = Environment.GetEnvironmentVariable("CosmosConnectionString");
+        private static readonly string _computerVisionApiKey = Environment.GetEnvironmentVariable("ComputerVision");
+        private static readonly string _storageConnectionString = Environment.GetEnvironmentVariable("StorageConnectionString");
 
-        private static readonly PetService petService = new PetService(cosmosConnectionString);
-        private static readonly HttpClient client = new HttpClient();
-        private static readonly CloudBlobClient storage = CloudStorageAccount.Parse(storageConnectionString).CreateCloudBlobClient();
+        private static readonly PetService _petService = new PetService(_cosmosConnectionString);
+        private static readonly HttpClient _client = new HttpClient();
+        private static readonly CloudBlobClient _storage = CloudStorageAccount.Parse(_storageConnectionString).CreateCloudBlobClient();
 
         [FunctionName("NewPetFunction")]
         public static async Task Run([QueueTrigger("newpets", Connection = "StorageConnectionString")]string queueItem, ILogger logger)
@@ -29,7 +29,7 @@ namespace MyNewHome.Functions
             var petFromQueue = JsonConvert.DeserializeObject<Pet>(queueItem);
 
             // Download image
-            var blob = new CloudBlockBlob(new Uri(petFromQueue.ImageUrl), storage);
+            var blob = new CloudBlockBlob(new Uri(petFromQueue.ImageUrl), _storage);
             var stream = await blob.OpenReadAsync();
 
             var binaryReader = new BinaryReader(stream);
@@ -38,8 +38,8 @@ namespace MyNewHome.Functions
             content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
 
             // Call Cognitive Services Computer Vision
-            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", computerVisionApiKey);
-            var response = await client.PostAsync(
+            _client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", _computerVisionApiKey);
+            var response = await _client.PostAsync(
                 "https://westeurope.api.cognitive.microsoft.com/vision/v1.0/generateThumbnail?width=400&height=300&smartCropping=true",
                 content);
 
@@ -50,9 +50,9 @@ namespace MyNewHome.Functions
                 await blob.UploadFromStreamAsync(thumbnail);
 
                 // Save url to Cosmos DB and publish
-                var pet = await petService.GetPetAsync(petFromQueue.Id, petFromQueue.Type);
+                var pet = await _petService.GetPetAsync(petFromQueue.Id, petFromQueue.Type);
                 pet.Published = true;
-                await petService.UpdatePetAsync(pet);
+                await _petService.UpdatePetAsync(pet);
             }
             else
             {
